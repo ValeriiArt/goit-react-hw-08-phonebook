9c6aace186046811ead4f68b5c90220553e6a279
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2022-07-30 17:43:17 +0300

    add navbar

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,29 +1,29 @@
-import { useSelector } from "react-redux";
-import ContactItem from "components/ContactItem";
+// import { useSelector } from "react-redux";
+// import ContactItem from "components/ContactItem";
 // import {useGetContactsQuery } from '../../store/contactsAPI';
 // import s from './ContactList.module.css'
 
 
 const ContactList = () => {
-  const search = useSelector(state => state.contacts.filter);
+  // const search = useSelector(state => state.contacts.filter);
   // const { data =[], isLoading } = useGetContactsQuery();
   
-  const findContact = () => {
-    const result = data && data.filter(contact =>
-      contact.name.toLowerCase().trim().includes(search.toLowerCase().trim())
-    );
-    return result;
-  };
+  // const findContact = () => {
+  //   const result = data && data.filter(contact =>
+  //     contact.name.toLowerCase().trim().includes(search.toLowerCase().trim())
+  //   );
+  //   return result;
+  // };
 
-  const contacts = findContact();
+  // const contacts = findContact();
 
-  if (isLoading) return <div>Loading...</div>
+  // if (isLoading) return <div>Loading...</div>
 
   return (
       <ul>
-      {contacts && contacts.map(contact =>
-        <ContactItem key={contact.id} {...contact} />
-      )}   
+      {/* {contacts && contacts.map(contact => */}
+        // <ContactItem key={contact.id} {...contact} />
+      {/* )}    */}
       </ul>
   )
 };
